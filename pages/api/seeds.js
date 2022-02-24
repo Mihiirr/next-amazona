9a86897ecc1a6edf0f84db0416186225bc119605
Mihir Patel <patelmihir5398@gmail.com>
2022-02-24 12:04:22 -0500

    changes for vercel

--- pages/api/seeds.js
+++ pages/api/seeds.js
@@ -1,19 +1,19 @@
 import nc from "next-connect";
-import Product from "../../models/Product";
-import User from "../../models/User";
-import data from "../../utils/data";
-import db from "../../utils/db";
+// import Product from "../../models/Product";
+// import User from "../../models/User";
+// import data from "../../utils/data";
+// import db from "../../utils/db";
 
 const handler = nc();
 
 handler.get(async (req, res) => {
-  await db.connect();
-  User.deleteMany();
-  User.insertMany(data.users);
-  Product.deleteMany();
-  Product.insertMany(data.products);
-  await db.disconnect();
-  res.send("message succeed");
+  // await db.connect();
+  // User.deleteMany();
+  // User.insertMany(data.users);
+  // Product.deleteMany();
+  // Product.insertMany(data.products);
+  // await db.disconnect();
+  res.send({ message: "already seeded" });
 });
 
 export default handler;
